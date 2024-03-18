8fe818b1f6bc5f3a23d84dc4456ac9ba7f865d84
Dhamu-Lmark <162978775+Dhamu-Lmark@users.noreply.github.com>
2024-03-18 18:46:30 +0530

    Update views.py

--- master-copier/master_copy/masterapp/views.py
+++ master-copier/master_copy/masterapp/views.py
@@ -30,7 +30,7 @@ def dashboard(request):
         return redirect('login')
 
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'index.html')
 
 def Account_Manager(request):
     return render(request, 'Account Manager.html')
